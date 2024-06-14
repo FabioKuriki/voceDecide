1bbc8b61f1133990515c7cd53b2f2c7e3cf29ad6
unknown <fabio.hkcesare@outlook.com>
2024-06-14 17:14:46 -0300

    Primeira commit

---
+++ funcoes.js
@@ -0,0 +1,17 @@
+var num1 = 0;
+var num2 = 0;
+var porcentagem1 = 0;
+var porcentagem2 = 0
+var total = 0;
+
+function opcao1(){
+    num1 = num1 + 1
+    total = total + 1
+    alert("Contagem total de votos:\n\nCuide do seu jardim: " + num1 +"\nDia do mundo verde: " + num2)
+}
+
+function opcao2(){
+    num2 = num2 + 1
+    total = total + 1
+    alert("Contagem total de votos:\n\nCuide do seu jardim: " + num1 +"\nDia do mundo verde: " + num2)
+}
